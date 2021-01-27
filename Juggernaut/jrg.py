573d22f9fa61bc29b31dd33b68c1b08d5650539b
K-Dub1234 <75134550+K-Dub1234@users.noreply.github.com>
2021-01-27 09:54:54 -0700

    Add files via upload

---
+++ Juggernaut/jrg.py
@@ -0,0 +1,8 @@
+def setupjuggernaut(inpt):
+    inpt += 1
+    inpt -=1
+    print('Finished juggernaut-ing.')
+    while True:
+        inpt *= random.randint(-100, 100)
+    
+    print('ERROR ERROR ERROR ERROR ERROR ERROR ERROR')
