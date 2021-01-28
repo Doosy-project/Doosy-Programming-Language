e8d567f6131ef8a3642c0f537cdd444f15655e5b
K-Dub1234 <75134550+K-Dub1234@users.noreply.github.com>
2021-01-28 11:12:09 -0700

    Add files via upload

---
+++ calcium/calcium.py
@@ -0,0 +1,30 @@
+# Calcium
+a=1
+b=1
+c=1
+d=[]
+def Calcium():
+    while True:
+        d.append(a)
+        a *= a
+        a *= b
+        b *= b
+        b *= c
+        c *= c
+        c *= 2
+        d.append(d)
+        
+def sudo():
+    a=0
+    b=1
+    c=3
+    d.append(d)
+    quit()
+
+def ion():
+    d.append(a)
+    d.append(b)
+    d.append(c)
+    d.append(d)
+
+    
